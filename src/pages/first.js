22e617ba24389810c491d52e2aad90f978a35775
xuemt <424307640@qq.com>
2019-02-16 10:39:51 +0800

    add content

--- src/pages/first.js
+++ src/pages/first.js
@@ -8,9 +8,9 @@ class First extends Comment {
         return (
             <div>
                 <h1>FEIRST</h1>
+                <h2>myfearture分支添加的内容</h2>
             </div>
         )
     }
 }
-
 export default First;
