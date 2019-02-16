473309e019f917cc3b5bbcf7827ce54ae25027b3
xuemt <424307640@qq.com>
2019-02-16 10:37:20 +0800

    add first

--- src/pages/first.js
+++ src/pages/first.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 
-const myComponent={
-    DatePicker: function DatePicker(props) {
-        return <div>Imagine a {props.color} datepicker here.</div>;
-      }
-}
 
-function BluedatePicker (){
-    return <myComponent.DatePicker color="blue"></myComponent.DatePicker>
-}
 
 class First extends Comment {
 
     render(){
-            
-
+        return (
+            <div>
+                <h1>FEIRST</h1>
+            </div>
+        )
     }
 }
 
-// export default First;
+export default First;
